6167853137b1ef5edfa8cad03fc2fca0feaad6ae
Fauzyah Hadirahma <97279376+fauzyahadirahma@users.noreply.github.com>
2022-02-13 17:33:55 +0700

    13 Feb 2022

---
+++ max.c
@@ -0,0 +1,27 @@
+/* File         : max.c */
+/* Penulis      : Fauzyah Hadirahma, email faubelajar@gmail.com */
+/* Deskripsi    : mencari nilai maksimum dari sebuah tabel yang berisi 10 nilai integer */
+
+#include <stdio.h>
+int main ()
+{
+    /* KAMUS */
+    int i;
+    int Tab [10] = {1, 50, 6, 200, 3, 100, 30, 8, 99, 100};
+    int max;
+    
+    /* ALGORITMA */
+    max = Tab [0];
+    
+    for (i=1; i<10; i++)
+        {
+            if (Tab [i] > max)
+            {
+                max = Tab [i];
+            }
+        }
+    
+    printf ("Nilai Max = %d", max);
+    
+    return 0;
+}
